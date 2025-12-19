83965dd4bf330ba19de623f542ec8aca40a8e431
monikha <monikha1807@gmail.com>
2025-12-19 12:52:15 +0530

    Fix seedData.js to use MONGO_URI

--- backend/seedData.js
+++ backend/seedData.js
@@ -6,7 +6,7 @@ const Network = require('./models/Network');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
