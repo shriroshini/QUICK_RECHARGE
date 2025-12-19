d58a024d1ba7ae5ccd80bebf796f1399dc78d111
monikha <monikha1807@gmail.com>
2025-12-19 11:19:05 +0530

    Update for Render deployment - MONGO_URI and CORS fixes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 // Middleware
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
